ec454f34b24e286c8deb712695bb2f0ae6bc8e57
root <2662252561@qq.com>
2021-09-01 11:16:42 +0800

    测试gitCommit

--- TourismLease-Kafka-8888/src/main/java/cn/doo/email/Consumer/ConsumerDemo.java
+++ TourismLease-Kafka-8888/src/main/java/cn/doo/email/Consumer/ConsumerDemo.java
@@ -25,10 +25,11 @@ public class ConsumerDemo {
 
     /**
      * 定义此消费者接收topics = "demo"的消息，与controller中的topic对应上即可
+     *
      * @param record 变量代表消息本身，可以通过ConsumerRecord<?,?>类型的record变量来打印接收的消息的各种信息
      */
     @KafkaListener(topics = "javatest")
-    public void listen (ConsumerRecord<?, ?> record) throws IOException, MessagingException {
+    public void listen(ConsumerRecord<?, ?> record) throws IOException, MessagingException {
         System.out.printf("topic is %s, offset is %d, value is %s \n", record.topic(), record.offset(), record.value());
 
         //获取数据
@@ -43,19 +44,19 @@ public class ConsumerDemo {
         if (hasKey) {
             //重复消费
             System.out.println("uuid = " + uuid + " 已经存在 , 成功拦截重复消费");
-        }else{
+        } else {
             //执行消费
 
             //0 : 登录验证码 , 1 : 发送维修提示
             Integer type = verify.getType();
             if (type == 0) {
-                emailUtils.sendVerifyEmail(verify.getEmail(),verify.getEmail(),verify.getCode());
+                emailUtils.sendVerifyEmail(verify.getEmail(), verify.getEmail(), verify.getCode());
             } else {
                 emailUtils.sendCheckEmail(verify.getEmail());
             }
 
             //kafka默认保存7天日志 所以要高出7天
-            redisUtil.setEx("email:" + verify.getUuid(),"success",8, TimeUnit.DAYS);
+            redisUtil.setEx("email:" + verify.getUuid(), "success", 8, TimeUnit.DAYS);
 
         }
 
